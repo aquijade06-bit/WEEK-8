1afce5946566768ed723d65501f1c76401008103
Grace <aquijade06@gmailcom>
2025-11-04 22:29:52 +0700

    Item class

--- src/WEEK8/Cash.java
+++ src/WEEK8/Cash.java
@@ -1,5 +1,19 @@
 package WEEK8;
 
-public class Cash {
+public class Cash extends Payment {
+    public Cash(Item item) {
+        super(item);
+    }
 
+    public int pay() {
+        if (isPaidOff) {
+            return 0;
+        }
+        isPaidOff = true;
+        return this.item.getPrice();
+    }
+
+    public String getClassName() {
+        return "CASH";
+    }
 }
--- src/WEEK8/Payment.java
+++ src/WEEK8/Payment.java
@@ -1,5 +1,44 @@
 package WEEK8;
 
 public abstract class Payment {
+    protected boolean isPaidOff;
+    protected Item item;
 
+    public abstract int pay();
+
+    public Payment() {
+        this.isPaidOff = false;
+        this.item = null;
+    }
+
+    public Payment(Item item) {
+        this.isPaidOff = false;
+        this.item = item;
+    }
+
+    public boolean isPaidOff() {
+        return isPaidOff;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public String getItemName() {
+        return item.getName();
+    }
+
+    public String getStatus() {
+        if (isPaidOff) {
+            return "FINISHED";
+        }
+        return "ON PROGRESS";
+    }
+
+    public int getRemainingAmount() {
+        if (isPaidOff) {
+            return 0;
+        }
+        return item.getPrice();
+    }
 }
